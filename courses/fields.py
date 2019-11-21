d80425551c89fab4557d4df6d5d69e1d745bffb9
Patricia Magee <xxxx@xxx.xxx>
2019-11-21 22:00:39 +0000

    second  commit

--- courses/fields.py
+++ courses/fields.py
@@ -3,12 +3,18 @@ from django.core.exceptions import ObjectDoesNotExist
 
 class OrderField(models.PositiveIntegerField):
     def __init__(self, for_fields=None, *args, **kwargs):
+        for x in args:
+            print(x)
+        for key, value in kwargs.items(): 
+            print ("%s == %s" %(key, value)) 
         self.for_fields = for_fields
         super(OrderField, self).__init__(*args, **kwargs)
     
     def pre_save(self, model_instance, add):
         if getattr(model_instance, self.attname) is None:
             # no current value
+            print('Model Instance',model_instance)
+            print('self.attname',self.attname)
             try:
                 qs = self.model.objects.all()
                 if self.for_fields:
@@ -18,11 +24,11 @@ class OrderField(models.PositiveIntegerField):
                     for field in self.for_fields}
                     qs = qs.filter(**query)
             # get the order of the last item
-            last_item = qs.latest(self.attname)
-            value = last_item.order + 1
+                last_item = qs.latest(self.attname)
+                value = last_item.order + 1
             except ObjectDoesNotExist:
                 value = 0
-        setattr(model_instance, self.attname, value)
+            setattr(model_instance, self.attname, value)
             return value
         else:
             return super(OrderField,self).pre_save(model_instance, add)
